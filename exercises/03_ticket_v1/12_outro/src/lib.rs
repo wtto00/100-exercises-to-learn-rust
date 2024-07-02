39098bf6afefc62eb749d56ea27b2859818263a0
wtto00 <wtto00@outlook.com>
2024-07-03 00:11:31 +0800

    04,00

--- exercises/03_ticket_v1/12_outro/src/lib.rs
+++ exercises/03_ticket_v1/12_outro/src/lib.rs
@@ -11,3 +11,62 @@
 // Integration here has a very specific meaning: they test **the public API** of your project.
 // You'll need to pay attention to the visibility of your types and methods; integration
 // tests can't access private or `pub(crate)` items.
+
+pub struct Order {
+    product_name: String,
+    quantity: u32,
+    unit_price: u32,
+}
+/// getter
+impl Order {
+    pub fn product_name(&self) -> &String {
+        &self.product_name
+    }
+    pub fn quantity(&self) -> &u32 {
+        &self.quantity
+    }
+    pub fn unit_price(&self) -> &u32 {
+        &self.unit_price
+    }
+}
+/// setter
+impl Order {
+    pub fn set_product_name(&mut self, product_name: String) {
+        self.product_name = product_name;
+    }
+    pub fn set_quantity(&mut self, quantity: u32) {
+        self.quantity = quantity;
+    }
+    pub fn set_unit_price(&mut self, unit_price: u32) {
+        self.unit_price = unit_price;
+    }
+}
+impl Order {
+    pub fn new(product_name: String, quantity: u32, unit_price: u32) -> Self {
+        assert_eq!(
+            product_name.len() > 0 && product_name.len() <= 300,
+            true,
+            "The product name can't be empty and it can't be longer than 300 bytes."
+        );
+        assert_eq!(
+            quantity > 0,
+            true,
+            "The quantity must be strictly greater than zero."
+        );
+        assert_eq!(
+            unit_price > 0,
+            true,
+            "The unit price is in cents and must be strictly greater than zero."
+        );
+        Self {
+            product_name,
+            quantity,
+            unit_price,
+        }
+    }
+}
+impl Order {
+    pub fn total(&self) -> u32 {
+        self.unit_price * self.quantity
+    }
+}
